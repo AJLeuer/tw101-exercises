678d95a321f1b05bdf6c0d83af2670a4d503630c
Adam J. Leuer <aleuer2@illinois.edu>
2016-02-19 08:16:40 -0600

    Refactored isosceles triangle method

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -1,11 +1,14 @@
 package com.thoughtworks.tw101.introductory_programming_exercises;
 
+import java.lang.StringBuilder ;
+
 public class DiamondExercises {
     public static void main(String[] args) {
-        drawAnIsoscelesTriangle(8, false);
-        //drawAnIsoscelesTriangle(10);
-        //drawADiamond(8);
-        //drawADiamondWithYourName(3);
+        //drawAnIsoscelesTriangle(8, false);
+
+        drawAnIsoscelesTriangle(10);
+        drawADiamond(8);
+        drawADiamondWithYourName(3);
     }
 
 //    Isosceles Triangle
@@ -14,48 +17,38 @@ public class DiamondExercises {
 //             ***
 //            *****
     private static void drawAnIsoscelesTriangle(int n) {
+        drawAnIsoscelesTriangle(n, false);
+    }
+
+
+    // Overloaded helper method for drawAnIsoscelesTriangle(int)
+    // Allows drawing an isosceles triangle upside-down
+    private static void drawAnIsoscelesTriangle(int n, boolean flipped) {
         /* the length of all rows is determined by the length of the final row, which is equal
            to 2x the total number of rows minus 1 */
         int sizeOfRows = (2 * n) - 1 ;
         int currentRowStartingPosition = sizeOfRows / 2 ;
 
+        StringBuilder output = new StringBuilder() ;
+
         for (int i = 0, currentRowAsterisksCount = 1 ; i < n ; i++) {
 
             for (int j = 0; j < currentRowStartingPosition ; j++) {
-                System.out.print(' ') ;
+                output.append(' ') ;
             }
 
             for (int j = 0; j < currentRowAsterisksCount; j++) {
-                System.out.print('*') ;
+                output.append('*') ;
             }
 
-            // we can just print a new line here
+            // we can just put a new line here
+            output.append('\n') ;
             System.out.println() ;
 
             currentRowStartingPosition -= 1 ;
             currentRowAsterisksCount += 2 ;
         }
-    }
-
-    private static void drawAnIsoscelesTriangle(int n, boolean flipped) {
-        /* the length of all rows is determined by the length of the first/final row, which is equal
-           to 2x the total number of rows minus 1 */
-        final int sizeOfRows = (2 * n) - 1 ;
-
-        int blankSpaces = flipped ? 0 : (sizeOfRows / 2) ;
-
-        for (int i = 0; i < sizeOfRows ; i++) {
-            for (int j = 0; j < blankSpaces  ; j++) {
-                System.out.print(' ') ;
-            }
-            for (int j = 0; j < (sizeOfRows - blankSpaces); j++) {
-                System.out.print('*') ;
-            }
-
-            System.out.println() ;
-
-            blankSpaces = flipped ? (blankSpaces + 2) : (blankSpaces - 2) ;
-        }
+        int i = 0 ;
     }
 
 //    Diamond
