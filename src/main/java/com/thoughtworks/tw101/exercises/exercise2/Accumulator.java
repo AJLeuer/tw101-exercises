d6d5e875b3a86de3fda538e53b82d1eeb8918bb4
Adam J. Leuer <ajleuer@gmail.com>
2016-02-22 05:19:45 -0600

    Implemented oddsum

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+
+    private int incrementCalls = 0 ;
+
+    public void increment() {
+        incrementCalls++ ;
+    }
+
+    public void total() {
+        System.out.println(incrementCalls) ;
+    }
 }
--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,16 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+
+        int sum = 0 ;
+
+        //if start is even, increase it by 1, otherwise keep it the same
+        start = ((start % 2) == 0) ? (start + 1) : start ;
+
+        for (int i = start ; i <= end ; i += 2) {
+            sum += i ;
+        }
+
+        return sum ;
     }
 }
