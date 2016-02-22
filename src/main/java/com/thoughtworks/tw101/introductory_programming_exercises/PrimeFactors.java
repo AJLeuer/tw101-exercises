d04e4605b629377522653f7d7303c437d2f0a4f8
Adam J. Leuer <ajleuer@gmail.com>
2016-02-22 04:05:25 -0600

    Added prime number generating methods

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,14 +6,64 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
+
     public static void main(String[] args) {
+
         List<Integer> primeFactors = generate(30);
     }
 
     private static List<Integer> generate(int n) {
+
         return null;
+    }
+
+    /**
+     * Method for finding prime numbers up to n. Implements the
+     * <a href="https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes">Sieve of Eratosthenes</a>.
+     */
+    private static List<Integer> findPrimes(int n) {
+
+        List<Integer> numbers = new ArrayList<Integer>() ;
+
+        //create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n)
+        for (int i = 2 ; i <= n ; i++) {
+            numbers.add(i) ;
+        }
+        
+        return findPrimesHelper(0, numbers) ;
+    }
+
+
+    /**
+     *
+     * @param index The position of the current integer used to eliminate non-primes in the list
+     * @param numbers A list of consecutive numbers, starting at 2, from which only the primes should be returned
+     * @return A List<Integer> with all the primes found in numbers
+     */
+    private static List<Integer> findPrimesHelper(int index, List<Integer> numbers) {
+
+        //base case
+        if (index >= (numbers.size() - 1)) {
+            return numbers ;
+        }
+
+        int p = numbers.get(index) ;
+
+        //remove all multiples of p found in the list (except p itself)
+        for (int i = 0; i < numbers.size() ; i++) {
+            //skip p itself in the list
+            if (numbers.get(i) == p) {
+                continue ;
+            }
+            if ((numbers.get(i) % p) == 0) {
+                numbers.remove(i) ;
+            }
+        }
+
+        return findPrimesHelper(index++, numbers) ;
     }
 }
